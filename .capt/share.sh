b6cf21b764627ade612b79847023431a2b5348dd
Micah Elliott <mde@micahelliott.com>
2023-10-16 16:19:43 -0700

    :sparkles: Add new checker, missingnewline

--- .capt/share.sh
+++ .capt/share.sh
@@ -1,8 +1,10 @@
 # Captain git-hook manager control file
 
+# Why is there no shellcheck? Doesn't seem to work well for too many things with Zsh.
 pre_commit=(
-    'doclint(md|mkd): mdl $CAPT_FILES_CHANGED'
+    'doclint(md|mkd): mdl $CAPT_FILES_CHANGED' # redundant with markdownlint but good for testing
     markdownlint
+    missingnewline
     "fixmes: git-confirm.sh"
 )
 
