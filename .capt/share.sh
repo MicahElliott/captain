23586385bdafe93bd09968b8bfc5fd589a6feab9
Micah Elliott <mde@micahelliott.com>
2023-10-26 05:49:44 -0700

    :sparkles: [misc] Add psuedo-descriptions, gitmessage, output
    Descriptions aren't real yet, but will likely use the format.
    
    Cleaned up some controls.
    
    Made br2msg a no-op on certain main-like branches.

--- .capt/share.sh
+++ .capt/share.sh
@@ -3,24 +3,23 @@
 # Why is there no shellcheck? Doesn't seem to work well for too many things with Zsh.
 pre_commit=(
     'hithere: hello $CAPT_FILES_CHANGED'
-    'doclint(md|mkd): mdl $CAPT_FILES_CHANGED' # redundant with markdownlint but good for testing
+    # redundant with markdownlint but for testing filters etc
+    'doclint(md|mkd): mdl $CAPT_FILES_CHANGED ## check markdown for violations'
     markdownlint
     # -markdownlint
     missingnewline
-    "fixmes: git-confirm.sh"
+    "fixmes: git-confirm.sh ## check for FIXMEs etc"
 )
 
 commit_msg=( msglint )
 
-prepare_commit_msg=( # br2msg
-    
-)
+prepare_commit_msg=( br2msg )
 
 post_commit=( colorquote )
 
 post_checkout=(
-    # migalert
-    # bundler
+    'fake-migalert: print "faking a checkout trigger" ## fake a migalert'
+    'fake-bundler:  print "pretending to run bundler" ## bundle resources'
 )
 
 # after a commit is modified by an amend or rebase
