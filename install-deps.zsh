afa831dc099f890a50317cdcc1450a23eb0cf197
Micah Elliott <mde@micahelliott.com>
2024-10-24 10:40:26 -0700

    :wrench: Move exes into bin dir
    Fixes: #4

--- install-deps.zsh
+++ install-deps.zsh
@@ -1,11 +1,15 @@
 #! /bin/zsh
 
 # rep, for running unit tests: https://github.com/eraserhd/rep
+print 'Installing rep for running fast clojure unit tests'
+cd src
 git clone https://github.com/eraserhd/rep
 cd rep
 cc -g -O2 -e rep rep.c
+cp rep ~/bin
 
 # gnu utils mac
+print 'Installing gnu utils for mac'
 brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep
 
 #
