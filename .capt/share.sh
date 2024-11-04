35d733cc33ab791aad08f8796010b61b93c7cd9b
Micah Elliott <mde@micahelliott.com>
2024-11-04 13:31:10 -0700

    :bomb: (#43) Attempt at interactive mode in magit
    Would need to figure out (again) how to get magit to accept user input.

--- .capt/share.sh
+++ .capt/share.sh
@@ -2,14 +2,16 @@
 
 # Why is there no shellcheck? Doesn't seem to work well for too many things with Zsh.
 pre_commit=(
-    'hithere: hello $CAPT_FILES_CHANGED'
+    # FIXME not picking up docstring
+    'hithere: hello $CAPT_FILES_CHANGED ## just say hello'
+    # 'xxx: git-confirm.sh'
     # redundant with markdownlint but for testing filters etc
-    'doclint(md|mkd): mdl $CAPT_FILES_CHANGED ## check markdown for violations'
+    # 'doclint(md|mkd): mdl $CAPT_FILES_CHANGED ## check markdown for violations'
     mdlint
-    fixcheck
+    -fixcheck
     # -markdownlint
     wscheck
-    "fixmes: git-confirm.sh ## check for FIXMEs etc"
+    # "fixmes: git-confirm.sh ## check for FIXMEs etc"
 )
 
 commit_msg=( msglint )
